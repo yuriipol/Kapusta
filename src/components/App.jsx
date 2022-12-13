210eb1a442b6a39a3519ee191a6bcecb6c37f018
yuriipol <104125040+yuriipol@users.noreply.github.com>
2022-12-13 16:48:42 +0200

    add reports page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import HomePage from 'pages/HomePage/HomePage';
+import ReportsPage from '../pages/ReportsPage/ReportsPage';
 
 export const App = () => {
   return (
     <div>
-      <HomePage />
+      {/* <HomePage /> */}
+      <ReportsPage />
     </div>
   );
 };
