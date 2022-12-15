23b32c579f82b50910ed18644173fb073ef733df
Halyna Melnykova <galyamelnykova@gmail.com>
2022-12-15 11:03:09 +0200

    select

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -8,25 +8,31 @@ import CreatableSelect from 'react-select/creatable';
 // export default () => <CreatableSelect isClearable options={colourOptions} />;
 import s from './Product.module.scss';
 
+const options = [
+  { value: 'Transport', label: 'Transport' },
+  { value: 'Products', label: 'Products' },
+  { value: 'vanilla', label: 'Vanilla' },
+];
+
 const Product = () => {
-  const customStyles = {
-    option: (provided, state) => ({
-      ...provided,
-      // color: state.isSelected ? 'yellow' : 'black',
-      // backgroundColor: state.isSelected ? 'white' : 'orange',
-    }),
-    control: provided => ({
-      ...provided,
-      marginTop: '0%',
-      marginLeft: '16px',
-      width: '280px',
-      height: '44px',
-      background: 'transparent',
-      borderColor: '#ffffff',
-      border: '2px solid #ffffff',
-      borderRadius: '0px 0px 10px 0px',
-    }),
-  };
+  // const customStyles = {
+  //   option: (provided, state) => ({
+  //     ...provided,
+  //     // color: state.isSelected ? 'yellow' : 'black',
+  //     // backgroundColor: state.isSelected ? 'white' : 'orange',
+  //   }),
+  //   control: provided => ({
+  //     ...provided,
+  //     marginTop: '0%',
+  //     marginLeft: '16px',
+  //     width: '280px',
+  //     height: '44px',
+  //     background: 'transparent',
+  //     borderColor: '#ffffff',
+  //     border: '2px solid #ffffff',
+  //     borderRadius: '0px 0px 10px 0px',
+  //   }),
+  // };
 
   return (
     <>
@@ -37,9 +43,10 @@ const Product = () => {
           placeholder="Product description"
         />
         <CreatableSelect
-          className={s.select}
-          styles={customStyles}
+          className={'product-select'}
+          classNamePrefix={'custom'}
           placeholder="Product category"
+          options={options}
         />
 
         {/* <img className={s.vector} src={Vector} alt="Vector" /> */}
