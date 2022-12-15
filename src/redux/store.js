aa64edbb9f9389042c99175ff2360f09e6a9772a
OlhaPetrenko <o.l.petr27@gmail.com>
2022-12-15 11:26:01 +0200

    add store

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+// import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
+import { authSlice } from './auth/auth-slice';
+
+const store = configureStore({
+  reducer: {
+    auth: authSlice.reducer,
+  },
+});
+export default store;
