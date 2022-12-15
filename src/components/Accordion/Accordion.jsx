11c43c2b516d2401b0b275c0a1649fee2a5e91cd
vovacelidze <louglescom@gmail.com>
2022-12-15 18:08:04 +0200

    fix button switch

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import Calendar from 'components/Calendar/Calendar';
 import Product from 'components/Product/Product';
+import Income from '../Income/Income'
 
 import s from './Accordion.module.scss';
 
@@ -11,18 +12,17 @@ const Accordion = () => {
     // запит за даними
   }, [isShow]);
 
-  const clickBtn = () => {
-    setIsShow(!isShow);
-  };
   return (
     // {isShow && }
     <div>
-      <button className={s.expences} type="button" onClick={clickBtn}>
-        Expenses
-      </button>
-      <button className={s.income} type="button" onClick={clickBtn}>
-        Income
-      </button>
+      <div>
+        <button className={s.expences} type="button" onClick={() => setIsShow(true)}>
+          Expenses
+        </button>
+        <button className={s.income} type="button" onClick={() => setIsShow(false)}>
+          Income
+        </button>
+      </div>
       {isShow ? (
         <div className={s.wrapper}>
           <Calendar />
