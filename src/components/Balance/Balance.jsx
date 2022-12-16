d91e559e25549a110219a86fd84282ddb6f8e092
Halyna Melnykova <galyamelnykova@gmail.com>
2022-12-16 10:12:51 +0200

    add handleSubmit in balance

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,16 +1,47 @@
+import { useState } from 'react';
+// import { useDispatch } from 'react-redux';
+
 import s from './Balance.module.scss';
 const Balance = () => {
+  const [state, setState] = useState({
+    balance: '',
+  });
+  const { balance } = state;
+  // const dispatch = useDispatch();
+
+  const handleChange = event => {
+    const { name, value } = event.currentTarget;
+    setState({ ...state, [name]: value });
+    // console.log(value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    // dispatch();
+    reset();
+  };
+
+  const reset = () => {
+    setState({ balance: '' });
+  };
+
   return (
-    <form className={s.form}>
+    <form className={s.form} onSubmit={handleSubmit}>
       <p className={s.text}>Balance:</p>
       <div className={s.wrapper}>
         <input
           type="number"
+          name="balance"
+          value={balance}
           title="Hello! To get started, enter the current balance of your account!"
           placeholder="00.00 UAN"
           className={s.input}
+          onChange={handleChange}
+          required
         />
-        <button className={s.confirm}>Confirm</button>
+        <button type="submit" className={s.confirm}>
+          Confirm
+        </button>
       </div>
     </form>
   );
