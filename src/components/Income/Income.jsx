30a0392b107d6e1986f187d5f24af5aa3678a607
vovacelidze <louglescom@gmail.com>
2022-12-16 23:31:43 +0200

    second section table and summary for desktop has been done

--- src/components/Income/Income.jsx
+++ src/components/Income/Income.jsx
@@ -1,24 +1,47 @@
 import React from 'react'
 import Table from '../Table/Table'
+import Summary from 'components/Summary/Summary'
 import Calendar from 'components/Calendar/Calendar'
 import s from './Income.module.scss'
 import Product_Tablet from 'components/Product/Product_Tablet'
+import useResizeScreen from '../../shared/hooks/useResizeScreen';
+
 
 const Income = () => {
-  return (
+  const {isTablet} = useResizeScreen();
+
+  if(isTablet){
+    return (
     <div className={s.income_Wrapper}>
       <div className={s.firstSection_Wrapper}>
         <Calendar />
         <Product_Tablet />
-        <button className={s.btn_Desktop}>Input</button>
-        <button className={s.btn_Desktop}>Clear</button>
       </div>
       <div className={s.secondSection_Wrapper}>
         <button className={s.btn}>Input</button>
         <button className={s.btn}>Clear</button>
       </div>
-      <Table />
+      <div className={s.test}>
+        <Table />
+      </div>
     </div>
+    )
+  }
+  return (
+    <div className={s.income_Wrapper}>
+    <div className={s.firstSection_Wrapper}>
+      <Calendar />
+      <Product_Tablet />
+      <button className={s.btn}>Input</button>
+      <button className={s.btn}>Clear</button>
+    </div>
+    <div className={s.secondSection_Wrapper}>
+    </div>
+    <div className={s.tabSumWrapper}>
+      <Table />
+      <Summary />
+    </div>
+  </div>
   )
 }
 
