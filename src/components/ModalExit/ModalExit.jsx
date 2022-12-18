5b4a86a62b02c0e8c65c9e3a0b5a19e793095a1a
Halyna Melnykova <galyamelnykova@gmail.com>
2022-12-18 11:53:44 +0200

    logout

--- src/components/ModalExit/ModalExit.jsx
+++ src/components/ModalExit/ModalExit.jsx
@@ -1,6 +1,17 @@
+import { useDispatch } from 'react-redux';
+import { logoutUser } from 'redux/auth/auth-operations';
+
 import s from './ModalExit.module.scss';
 
 const ModalExit = ({ close }) => {
+  const dispatch = useDispatch();
+
+  const logOut = () => {
+    dispatch(logoutUser());
+    // localStorage.removeItem('persist:auth');
+    close();
+  };
+
   const closeModal = ({ target, currentTarget, code }) => {
     if (target === currentTarget || code === 'Escape') {
       close();
@@ -11,7 +22,9 @@ const ModalExit = ({ close }) => {
       <div className={s.modal}>
         <button className={s.close} type="button" onClick={closeModal}></button>
         <p className={s.text}>Do you really want to leave?</p>
-        <button className={s.buttonYes}>Yes</button>
+        <button className={s.buttonYes} onClick={() => logOut()}>
+          Yes
+        </button>
         <button className={s.buttonNo} onClick={closeModal}>
           No
         </button>
